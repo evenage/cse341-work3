51bb80646d55a78710bcfe5a87e45dcd7ecb99d3
evenage <charue.vyago2019@gmail.com>
2025-03-30 02:06:22 +0200

    changes in indes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = require("express").Router();
 // Swagger documentation route
 router.use("/", require("./swagger"));
 
+router.get("/", (req, res) => {
+    //#swagger-tags: ['Hello from routes!']
+    res.send("Hello, from routes!");
+  });
+  
+
 // Cars and owners routes
 router.use("/cars", require("./cars"));
 router.use("/owner", require("./owner"));
